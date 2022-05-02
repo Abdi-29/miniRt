b9c6d3b283577a0ad18872e4b8b82a7c4e63c665
Stijn <stijn.d2000@hotmail.com>
2022-05-02 19:15:54 +0200

    Added to parsing lines

--- srcs/parsing/parse_lines.c
+++ srcs/parsing/parse_lines.c
@@ -12,12 +12,29 @@
 
 #include "../../includes/lib.h"
 #include "../../includes/data_struct.h"
+#include "../../includes/util.h"
+
+int	arr_len(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (*line++)
+		i++;
+	return (i);
+}
 
 static void	parse_line(char *line, t_minirt_data *data)
 {
 	char	**arr;
+	int		len;
 
 	arr = ft_split(line, ft_iswhite_space);
+	if (arr == NULL)
+		err_exit(1, "Error\nRan out of memory.\n");
+	len = arr_len(line);
+	if (len < 3)
+		err_exit(1, "Error\nNot enough arguments on the following line: [%s]\n", line);
 }
 
 void	parse_lines(t_list **head, t_minirt_data *data)
