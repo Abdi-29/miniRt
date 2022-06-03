29111a63c75cfbe534384c0be3b821330574b430
Abdoulaye Ba <abba@f0r3s24.codam.nl>
2022-06-03 21:26:51 +0200

    moved the header into includes dir

---
+++ includes/ray.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ray.h                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sappunn <sappunn@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/05/06 19:14:58 by sappunn       #+#    #+#                 */
+/*   Updated: 2022/05/06 19:14:58 by sappunn       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RAY_H
+# define RAY_H
+# include "xyz.h"
+# include "data_struct.h"
+# include "bool.h"
+
+typedef struct s_obj_data
+{
+	t_rgb		color;
+	double		distance;
+	t_bool		has_color;
+	t_sphere	*sphere;
+	t_plane		*plane;
+	t_cylinder	*cylinder;
+	double		angle;
+}	t_obj_data;
+
+typedef struct s_ray
+{
+	t_xyz		origin;
+	t_xyz		direction;
+}	t_ray;
+
+typedef struct s_screen
+{
+	double	screen_x;
+	double	screen_y;
+	double	pixel_camera_x;
+	double	pixel_camera_y;
+	double	image_ratio;
+}				t_screen;
+
+int		ray_color(t_ray ray, t_minirt_data *data);
+t_ray	create_ray(t_minirt_data *data, int i, int j);
+t_xyz	init_coords(double x, double y, double z);
+
+void	loop_cylinder(t_ray ray, t_list *entry, t_obj_data *obj);
+void	loop_plane(t_ray ray, t_list *entry, t_obj_data *obj);
+void	loop_sphere(t_ray ray, t_list *entry, t_obj_data *obj);
+void	loop_objects(t_ray ray, t_minirt_data *data, t_obj_data *obj);
+
+int		tem(t_minirt_data *data, t_obj_data *obj, t_ray old_ray);
+int		get_color(t_rgb rgb, t_minirt_data *data);
+
+#endif
---
+++ includes/vector.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   vector.h                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sappunn <sappunn@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/05/06 19:57:42 by sappunn       #+#    #+#                 */
+/*   Updated: 2022/05/06 19:57:42 by sappunn       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_H
+# define VECTOR_H
+# include "xyz.h"
+
+t_xyz	plus(t_xyz a, t_xyz b);
+t_xyz	minus(t_xyz a, t_xyz b);
+t_xyz	multiplication(t_xyz a, t_xyz b);
+t_xyz	division(t_xyz a, double t);
+double	magnitute(t_xyz a);
+double	dot(t_xyz a, t_xyz b);
+t_xyz	mult_xyz_dub(t_xyz a, double t);
+t_xyz	unit_vector(t_xyz a);
+t_xyz	normalized(t_xyz a);
+double	length(t_xyz a);
+t_xyz	cross(t_xyz a, t_xyz b);
+double	distance(t_xyz one, t_xyz two);
+
+void	mat_init_axes(t_mat *in, const t_xyz forward);
+t_xyz	mat_mult_dir(t_mat in, t_xyz dir);
+
+#endif
