4145554615f3975e237d8b36708d548e1b05e70e
Abdoulaye Ba <abba@f0r3s24.codam.nl>
2022-06-10 18:12:33 +0200

    improved cylinder function

--- srcs/intesections/cylinder.c
+++ srcs/intesections/cylinder.c
@@ -13,7 +13,6 @@
 #include "../../includes/ray.h"
 #include <math.h>
 #include "../../includes/vector.h"
-#include <stdio.h>
 
 static t_ray	translate_ray(t_ray ray, t_xyz normal, t_xyz origin)
 {
@@ -22,9 +21,8 @@ static t_ray	translate_ray(t_ray ray, t_xyz normal, t_xyz origin)
 	t_xyz	up;
 	t_xyz	forward;
 
-	//printf("%f %f %f\n", normal.xyz[0], normal.xyz[1], normal.xyz[2]);
 	forward = normalized(cross(normal, (t_xyz){{1, 0, 0}}));
-	if (length(forward) == 0.0)
+	if (isnan(length(forward)))
 		forward = (t_xyz){{0, 1, 0}};
 	up = normalized(cross(forward, normal));
 	inverse.mat[2][0] = forward.xyz[0];
@@ -36,12 +34,10 @@ static t_ray	translate_ray(t_ray ray, t_xyz normal, t_xyz origin)
 	inverse.mat[0][0] = up.xyz[0];
 	inverse.mat[0][1] = up.xyz[1];
 	inverse.mat[0][2] = up.xyz[2];
-
 	new_ray.origin = minus(ray.origin, origin);
 	new_ray.origin = mat_mult_dir(inverse, new_ray.origin);
 	new_ray.origin = plus(new_ray.origin, origin);
-
-	//inverse = mat_transpose(inverse);
+//	inverse = mat_transpose(inverse);
 	new_ray.direction = normalized(mat_mult_dir(inverse, ray.direction));
 	return (new_ray);
 }
@@ -50,8 +46,9 @@ static t_ray	get_delta(t_cylinder *cylinder, t_ray ray)
 {
 	t_xyz	oc;
 	t_ray	new_ray;
-	t_xyz	tmp = (t_xyz){{0, 1, 0}};
+	t_xyz	tmp;
 
+	tmp = (t_xyz){{0, 1, 0}};
 	new_ray.origin = ray.origin;
 	new_ray.direction = cross(ray.direction, tmp);
 	oc = minus(ray.origin, cylinder->xyz);
@@ -65,12 +62,30 @@ static t_ray	get_delta(t_cylinder *cylinder, t_ray ray)
 	return (new_ray);
 }
 
+static t_bool	test(t_ray ray, t_cylinder *cylinder, double len)
+{
+	double	height;
+	t_xyz	p;
+	t_xyz	n;
+	t_xyz	op;
+
+	p = plus(ray.origin, mult_xyz_dub(ray.direction, len));
+	n = (t_xyz){{p.xyz[0] - cylinder->xyz.t_s_xyz.x, 0.0, p.xyz[2] - cylinder->xyz.t_s_xyz.z}};
+	op = plus(cylinder->xyz, n);
+	height = length(minus(p, op));
+	if (height < cylinder->height / 2.)
+	{
+		cylinder->distance = len;
+		cylinder->normal = mult_xyz_dub(normalized(n), -1.);
+		return (TRUE);
+	}
+	return (FALSE);
+}
 
 static t_bool	cylinder_intersect(t_cylinder *cylinder, t_ray ray)
 {
 	double	len_one;
 	double	len_two;
-
 
 	ray = translate_ray(ray, cylinder->vector, cylinder->xyz);
 	get_delta(cylinder, ray);
@@ -82,33 +97,11 @@ static t_bool	cylinder_intersect(t_cylinder *cylinder, t_ray ray)
 		/ (2 * cylinder->delta.delta[0]);
 	if (len_one < 0)
 		return (FALSE);
-
-	t_xyz	p = plus(ray.origin, mult_xyz_dub(ray.direction, len_one));
-	t_xyz	n = (t_xyz){{p.xyz[0] - cylinder->xyz.t_s_xyz.x, 0.0, p.xyz[2] - cylinder->xyz.t_s_xyz.z}};
-
-	t_xyz	op = plus(cylinder->xyz, n);
-	float	height = length(minus(p, op));
-
-	if (height < cylinder->height / 2.)
-	{
-		cylinder->distance = len_one;
-		cylinder->normal = normalized(n);
-		cylinder->normal = mult_xyz_dub(normalized(n), -1.);
+	if (test(ray, cylinder, len_one))
 		return (TRUE);
-	}
 	len_two = (-cylinder->delta.delta[1] + sqrt(cylinder->delta.t_delta.discriminant))
 		/ (2 * cylinder->delta.delta[0]);
-	p = plus(ray.origin, mult_xyz_dub(ray.direction, len_two));
-	n = (t_xyz){{p.xyz[0] - cylinder->xyz.t_s_xyz.x, 0.0, p.xyz[2] - cylinder->xyz.t_s_xyz.z}};
-	op = plus(cylinder->xyz, n);
-	height = length(minus(p, op));
-	if (height < cylinder->height / 2.)
-	{
-		cylinder->distance = len_two;
-		cylinder->normal = mult_xyz_dub(normalized(n), -1.);
-		return (TRUE);
-	}
-	return (FALSE);
+	return (test(ray, cylinder, len_two));
 }
 
 void	loop_cylinder(t_ray ray, t_list *entry, t_obj_data *obj)
